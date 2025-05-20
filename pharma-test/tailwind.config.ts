f5d6f5ddac07d8c08db251e6e10164df59b15dae
ohendarko <ohendarko@gmail.com>
2025-05-20 22:28:35 +0000

    latest changes

--- pharma-test/tailwind.config.ts
+++ pharma-test/tailwind.config.ts
@@ -4,8 +4,11 @@ const config: Config = {
   darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/components/ui/**/*.{js,ts,jsx,tsx,mdx}",
