5d1eeb6ca8d132978c0338524784d0e1a99e46af
ohendarko <ohendarko@gmail.com>
2025-07-16 03:19:22 +0000

    google verification

--- c3initiative/app/layout.tsx
+++ c3initiative/app/layout.tsx
@@ -5,12 +5,14 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ThemeToggle } from "@/components/theme-toggle"
 import { Toaster } from "@/components/ui/toaster"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "C3 Initiative - Smart Cervical Cancer Education",
   description: "C3 Initiative's e-learning platform for cervical cancer education and awareness",
+  
 }
 
 export default function RootLayout({
@@ -20,6 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta name="google-site-verification" content="zGMyfhH9SW9Q0rGib_uc7PpwuyuXaN6ZDu607DqwVyE" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <ThemeToggle />
