cf765c329c4f875988f380ea1484c276e7ea8242
ohendarko <ohendarko@gmail.com>
2025-07-16 03:25:27 +0000

    google verification lets try this

--- c3initiative/app/layout.tsx
+++ c3initiative/app/layout.tsx
@@ -12,6 +12,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "C3 Initiative - Smart Cervical Cancer Education",
   description: "C3 Initiative's e-learning platform for cervical cancer education and awareness",
+  verification: {
+    google: "zGMyfhH9SW9Q0rGib_uc7PpwuyuXaN6ZDu607DqwVyE",
+  },
   
 }
 
@@ -22,9 +25,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
+      {/* <Head>
         <meta name="google-site-verification" content="zGMyfhH9SW9Q0rGib_uc7PpwuyuXaN6ZDu607DqwVyE" />
-      </Head>
+      </Head> */}
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <ThemeToggle />
