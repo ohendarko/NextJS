c2f01c87582e98530e89df51c9657fbdf3f6f1f6
ohendarko <ohendarko@gmail.com>
2025-07-16 15:26:16 +0000

    added terms and privacy policy

---
+++ c3initiative/app/(learner)/privacy/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Privacy = () => {
+  return (
+    <div className="mt-20">Privacy Policy</div>
+  )
+}
+
+export default Privacy
---
+++ c3initiative/app/(learner)/terms/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Terms = () => {
+  return (
+    <div className="mt-20">Terms and Conditions</div>
+  )
+}
+
+export default Terms
