1dfb3e0e643b267a1eae98aad29132e8a2a9404a
ohendarko <ohendarko@gmail.com>
2025-10-12 21:41:00 -0400

    all systems operating

--- yc_directory/app/(root)/page.tsx
+++ yc_directory/app/(root)/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+    <section className="pink_container">
       <h1 className="heading">Home</h1>
+
+    </section>
     </>
   );
 }
